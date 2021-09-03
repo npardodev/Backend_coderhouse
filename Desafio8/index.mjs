8303082c786a0dfa09f164ff0b83a2cdbc34b580
npardo <npardo.dev@gmail.com>
2021-09-03 17:05:06 -0300

    Desafio 9

--- Desafio8/index.mjs
+++ Desafio8/index.mjs
@@ -1,9 +1,10 @@
 import express from 'express';
+import { Products } from './products.mjs'
 import path from 'path';
 const __dirname = path.resolve();
 
 const app = express();
-const PORT = 8080;
+const PORT = 8085;
 
 const PATH_LIST_ALL_PRODUCTS = '/api/productos/listar';
 const PATH_LIST_DETAIL_PRODUCT = '/api/productos/listar/:id';
@@ -13,52 +14,46 @@ const ERROR_PRODUCT_NOT_EXIST = { error: 'Producto no encontrado ' };
 const SUCCESS_PRODUCT_NOT_FOUND = { error: 'No hay productos cargados ' };
 const ERROR_INVALID_DATA = { error: 'Datos ingresados inválidos ' };
 
-const productos = [];
 let idToFind = 0;
 
-const postPaths = [{
-    rute: PATH_LIST_SAVE_PRODUCT,
-    response: `Producto incorporado`,
-}]
-
-const getPaths = [{
-        rute: PATH_LIST_ALL_PRODUCTS,
-        response: productos.length == 0 ? SUCCESS_PRODUCT_NOT_FOUND : `Lista de productos:{${ JSON.stringify(productos)}}`,
-    },
-    {
-        rute: PATH_LIST_DETAIL_PRODUCT,
-        response: `Producto:{${ JSON.stringify(productos.find(element => element.id == idToFind))}}`,
-    },
-]
+let productos = [];
+const myProducts = new Products(productos);
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extened: true }));
 app.use(express.static(__dirname + '/public'));
 
-for (let i = 0, len = getPaths.length; i < len; i++) {
 
-    app.get(getPaths[i].rute, (req, res) => {
-        console.log('request GET recibido');
-        res.json(getPaths[i].response);
-    })
-}
+app.get(PATH_LIST_ALL_PRODUCTS, (req, res) => {
+    console.log('request GET recibido');
+    res.send(myProducts.list() == 0 ? SUCCESS_PRODUCT_NOT_FOUND : myProducts.list());
+})
 
-app.post(postPaths[0].rute, (req, res) => {
+
+app.get(PATH_LIST_DETAIL_PRODUCT, (req, res) => {
+    console.log('request GET recibido');
+    res.send(`Producto:{${ JSON.stringify(productos.find(element => element.id == idToFind))}}`);
+})
+
+
+app.post(PATH_LIST_SAVE_PRODUCT, (req, res) => {
 
     console.log('request POST recibido');
-    const message = req.body;
+    const mssge = req.body;
+    if (mssge !== '') {
 
-    if (message !== '') {
+        const { msgTitle, msgPrice, msgThum } = req.body;
 
         const newProduct = {
-            title: `${message.title}`,
-            price: `${message.price}`,
-            thumbnail: `${message.img}`,
+            title: `${msgTitle}`,
+            price: `${msgPrice}`,
+            thumbnail: `${msgThum}`,
         };
 
-        productos.push(newProduct);
-        res.json(postPaths[0].response);
+        myProducts.add(newProduct);
+        console.log(myProducts.productos);
+        res.json(`Producto incorporado`);
 
     } else {
         console.log("Campos ingresados vacios");
